2e52940199025b5d215a25ed5769812bf6e68e3a
Ol Gen <oggen18@gmail.com>
2020-08-10 19:02:44 +0300

    5. PsqlStore

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,144 @@
+package ru.job4j.grabber;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store, AutoCloseable {
+
+    private Connection conn;
+
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("jdbc.driver"));
+            conn = DriverManager.getConnection(
+                    cfg.getProperty("jdbc.url"),
+                    cfg.getProperty("jdbc.username"),
+                    cfg.getProperty("jdbc.password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private Calendar convertToCalendar(Timestamp ts) {
+        Calendar javaDate = Calendar.getInstance();
+        javaDate.setTimeInMillis(ts.getTime());
+        return javaDate;
+    }
+
+    @Override
+    public void save(Post post) {
+        try {
+            PreparedStatement st = conn.prepareStatement(
+                    "insert into post("
+                            + "name, text, link, created, author, author_URL, last_commented)"
+                    + "values(?, ?, ?, ?, ?, ?, ?)");
+            st.setString(1, post.getVacancyName());
+            st.setString(2, post.getVacancyDesc());
+            st.setString(3, post.getVacancyURL());
+            st.setTimestamp(4, new Timestamp(post.getDateOfCreation().getTime().getTime()));
+            st.setString(5, post.getAuthorName());
+            st.setString(6, post.getAuthorURL());
+            st.setTimestamp(7, new Timestamp(post.getDateOfLatestComment().getTime().getTime()));
+            st.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> list = new ArrayList<>();
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select * from post");
+            while (rs.next()) {
+                Post post = new Post();
+                post.setVacancyName(rs.getString("name"));
+                post.setVacancyDesc(rs.getString("text"));
+                post.setVacancyURL(rs.getString("link"));
+                post.setDateOfCreation(convertToCalendar(rs.getTimestamp("created")));
+                post.setAuthorName(rs.getString("author"));
+                post.setAuthorURL(rs.getString("author_url"));
+                post.setDateOfLatestComment(convertToCalendar(rs.getTimestamp("last_commented")));
+                list.add(post);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    @Override
+    public Post findById(String id) {
+        Post post = null;
+        try {
+            PreparedStatement st = conn.prepareStatement("select * from post where id = ?");
+            st.setInt(1, Integer.parseInt(id));
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                post = new Post();
+                post.setId(String.valueOf((rs.getInt("id"))));
+                post.setVacancyName(rs.getString("name"));
+                post.setVacancyDesc(rs.getString("text"));
+                post.setVacancyURL(rs.getString("link"));
+                post.setDateOfCreation(convertToCalendar(rs.getTimestamp("created")));
+                post.setAuthorName(rs.getString("author"));
+                post.setAuthorURL(rs.getString("author_url"));
+                post.setDateOfLatestComment(convertToCalendar(rs.getTimestamp("last_commented")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (conn != null) {
+            conn.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        Properties cfg = new Properties();
+        try (InputStream in = PsqlStore.class.getClassLoader()
+             .getResourceAsStream("grabber.properties")) {
+            cfg.load(in);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+
+        PsqlStore psqlStore = new PsqlStore(cfg);
+        SqlRuGrab sqlRuGrab = new SqlRuGrab();
+        List<Post> posts = sqlRuGrab.list("https://www.sql.ru/forum/job-offers/");
+        Post fifthPost = posts.get(4);
+        Post tenthPost = posts.get(10);
+        psqlStore.save(fifthPost);
+        psqlStore.save(tenthPost);
+        Post fifth = psqlStore.findById("1");
+        Post tenth = psqlStore.findById("2");
+        psqlStore.showInfo(fifth);
+        psqlStore.showInfo(tenth);
+
+        List<Post> list = psqlStore.getAll();
+        list.forEach(psqlStore::showInfo);
+    }
+
+    public void showInfo(Post post) {
+        System.out.println("id в базе: " + post.getId());
+        System.out.println("Название вакансии: " + post.getVacancyName());
+        System.out.println("URL вакансии: " + post.getVacancyURL());
+        System.out.println("Описание вакансии: " + post.getVacancyDesc());
+        System.out.println("Дата создания: " + post.getDateOfCreation().getTime());
+        System.out.println("Имя автора: " + post.getAuthorName());
+        System.out.println("Информация об авторе: " + post.getAuthorURL());
+        System.out.println("Дата последнего комментария к вакансии: "
+                + post.getDateOfLatestComment().getTime());
+        System.out.println();
+    }
+}
---
+++ src/main/java/ru/job4j/grabber/SqlRuGrab.java
@@ -0,0 +1,134 @@
+package ru.job4j.grabber;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+
+public class SqlRuGrab implements Parse {
+
+    private final Map<String, Integer> months = new java.util.HashMap<>();
+
+    public SqlRuGrab() {
+        fillingMap();
+    }
+
+    private void fillingMap() {
+        Calendar today = Calendar.getInstance();
+        months.put("янв", 0);
+        months.put("фев", 1);
+        months.put("мар", 2);
+        months.put("апр", 3);
+        months.put("май", 4);
+        months.put("июн", 5);
+        months.put("июл", 6);
+        months.put("авг", 7);
+        months.put("сен", 8);
+        months.put("окт", 9);
+        months.put("ноя", 10);
+        months.put("дек", 11);
+        months.put("сегодня", today.get(Calendar.DAY_OF_MONTH));
+        months.put("вчера", today.get(Calendar.DAY_OF_MONTH) - 1);
+    }
+
+    private int getTrueYear(int shortYear, Calendar today) {
+        int previousCentury = 1900;
+        int thisCentury = 2000;
+        int thisYear = today.get(Calendar.YEAR);
+        if (thisYear - shortYear >= thisCentury) {
+            return thisCentury + shortYear;
+        } else {
+            return previousCentury + shortYear;
+        }
+    }
+
+    public Calendar dateConvert(String input) {
+        String[] dateAndTime = input.split(", ");
+        String date = dateAndTime[0];
+        String time = dateAndTime[1];
+        int colon = time.indexOf(":");
+        int hour = Integer.parseInt(time.substring(0, colon));
+        int minute = Integer.parseInt(time.substring(colon + 1));
+        int day;
+        int month;
+        int year;
+        Calendar javaDate = Calendar.getInstance();
+        if (date.equals("сегодня") || date.equals("вчера")) {
+            month = javaDate.get(Calendar.MONTH);
+            day = months.get(date);
+            year = javaDate.get(Calendar.YEAR);
+            javaDate.set(year, month, day, hour, minute);
+            return javaDate;
+        }
+        String[] dayMonthYear = date.split(" ");
+        day = Integer.parseInt(dayMonthYear[0]);
+        month = months.get(dayMonthYear[1]);
+        year = getTrueYear(Integer.parseInt(dayMonthYear[2]), javaDate);
+        javaDate.set(year, month, day, hour, minute);
+        return javaDate;
+    }
+
+    @Override
+    public List<Post> list(String link)  {
+        List<Post> posts = new ArrayList<>();
+        try {
+            for (int p = 1; p <= 5; p++) {
+                String page = String.valueOf(p);
+                String url = link.concat(page);
+                Document doc = Jsoup.connect(url).get();
+                Elements row = doc.select(".postslisttopic");
+                Elements dates = doc.select(".altCol");
+                for (int i = 0, k = 1; i < row.size(); i++, k += 2) {
+
+                    Element tdPosts = row.get(i);
+                    Element href = tdPosts.child(0);
+                    Element dateOfLatestComment = dates.get(k);
+                    String vacancyURL = href.attr("href");
+                    String vacancyName = href.text();
+                    Post post = detail(vacancyURL);
+                    Calendar javaDate = dateConvert(dateOfLatestComment.text());
+                    post.setDateOfLatestComment(javaDate);
+                    post.setVacancyName(vacancyName);
+                    post.setVacancyURL(vacancyURL);
+                    posts.add(post);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Post detail(String link) {
+        Post post = new Post();
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements descs = doc.select(".msgBody");
+            Elements msgFooter = doc.select(".msgFooter");
+
+            Element descUneven = descs.get(1);
+            post.setVacancyDesc(descUneven.text());
+
+            Element descEven = descs.get(0);
+            Element author = descEven.child(0);
+            String url = author.attr("href");
+            String name = author.text();
+            post.setAuthorName(name);
+            post.setAuthorURL(url);
+
+            String dateAndStuff = msgFooter.get(0).text();
+            int endingOfDate = dateAndStuff.indexOf(" [");
+            String date = dateAndStuff.substring(0, endingOfDate);
+            post.setDateOfCreation(dateConvert(date));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+}
